426f6eea749d4889539f02aaca4fe067a7aa4832
farhanfahmid <fahmidfarhan274@gmail.com>
2025-04-04 15:39:54 +0600

    Remove unnecessary lines

--- email_sender.py
+++ email_sender.py
@@ -1,9 +1,4 @@
 import smtplib, ssl
-import streamlit as st
-
-# # Your Formspree endpoint
-# FORMSPREE_URL = "https://formspree.io/f/mnnpnozd"
-
 
 def send_email(message):
    host = "smtp.gmail.com"
